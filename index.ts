16a5326975e17732d73148897de769cae63c37f8
Mahamali0 <164751876+Mahamali0@users.noreply.github.com>
2024-03-24 23:10:40 +0500

    Add files via upload
    my simple calculator

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// printing a welcome message 
+console.log("/n/tWellcome To / Codewithmaham/' -CLI Simple Calculator/n ");
+
+// Asking Question From Users through inquirer
+
+let answer = await inquirer.prompt([
+{message :"Enter your First Number", type : "number", name: "firstNumber"},
+
+{message :"Enter Your Second Number", type: "number", name: "secondNumber"},
+{
+    message : "Select one Operator to perform Operations",
+    type : "list",
+    name : "operator",
+    choices : [ "Addition" , "Subtraction", "Multiplication", "Division"],
+},
+]);
+
+// conditional statements if-Else
+if(answer.operator === "Addition"){
+   console.log(answer.firstNumber + answer.secondNumber )   
+}
+else if (answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else{
+   console.log("Invalid Input") 
+}
+
+                                                              
+  
